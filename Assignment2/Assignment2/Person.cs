df4ff352ab161bd2c38133a9a0d4a61134e0983b
mcmejorado <S00735680@ESS-TLC-046.acad.tri-c.edu>
2016-02-17 13:07:33 -0500

    Assignment2

---
+++ Assignment2/Assignment2/Person.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    public class Person
+    {
+        public int Age;
+        public string FirstName;
+        public string LastName;
+        //public Person Spouse = new Person();
+        public string spouseName;
+        public int spouseAge;
+
+        public static double SumOfAllAges;
+
+         public void CollectData(string personNumber)
+         {
+             System.Console.WriteLine("");
+             System.Console.WriteLine("[ Enter information for " + personNumber + " ]");
+             System.Console.WriteLine("");
+
+             System.Console.Write("Enter Your First Name            : "); 
+             this.FirstName = System.Console.ReadLine();
+             
+             System.Console.Write("Enter Your Last Name             : ");
+             this.LastName = System.Console.ReadLine();
+
+             System.Console.Write("Enter Your Age                   : ");
+             this.Age = int.Parse(System.Console.ReadLine());
+
+             System.Console.Write("Enter Your Spouse's First Name   : ");
+             this.spouseName = System.Console.ReadLine();
+
+             System.Console.Write("Enter Your Spouse's Age          : ");
+             this.spouseAge = int.Parse(System.Console.ReadLine());
+            
+             System.Console.WriteLine("");
+         } 
+
+        public string GetFullName(string name, string lastname, int age)
+        {
+            return lastname +", " + name + "(" + age +")";
+        }
+  
+        public void PrintNameAndAge(string fullName)
+        {
+            System.Console.Write(fullName);
+           
+        }
+
+        public void GetAverageAge()
+        {
+            
+        }
+
+
+    }
+}
---
+++ Assignment2/Assignment2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person P1 = new Person();
+            Person P2 = new Person();
+
+            P1.CollectData("p1");
+            System.Console.ReadKey();
+
+            P2.CollectData("p2");
+            System.Console.ReadKey();
+
+            string FullNameP1 = P1.GetFullName(P1.FirstName, P1.LastName, P1.Age);
+            string FullNameP2 = P2.GetFullName(P2.FirstName, P2.LastName, P2.Age);
+
+            System.Console.WriteLine("[Printing Results]");
+            System.Console.WriteLine("");
+            P1.PrintNameAndAge(FullNameP1);
+            P2.PrintNameAndAge(FullNameP2);
+
+        }
+    }
+}
