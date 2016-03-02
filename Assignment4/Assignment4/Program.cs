d5cc8446c27f448cad276fb559e95a4ca1befe56
mcmejorado <mariac.mejorado@gmail.com>
2016-03-02 12:40:05 -0500

    Constants declaration

---
+++ Assignment4/Assignment4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double matineeChild = 3.99;
+            const double matineeAdult = 5.99;
+            const double matineeSenior = 4.50;
+            const double eveningChild = 6.99;
+            const double eveningAdult = 10.99;
+            const double eveningSenior = 8.50
+            const double smallSoda = 3.50;
+            const double largeSoda = 5.99;
+            const double hotDog = 3.99;
+            const double popCorn = 4.50;
+            const double candy = 1.99;
+
+        }
+    }
+}
