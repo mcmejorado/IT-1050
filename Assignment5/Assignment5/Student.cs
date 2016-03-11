2cfbde6974375df7fa1433603d8ce6836c1b5048
mcmejorado <mariac.mejorado@gmail.com>
2016-03-11 13:10:12 -0500

    Classes
    Classes Declaration

---
+++ Assignment5/Assignment5/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5
+{
+    class Student
+    {
+        private string name;
+        private string sNumber;
+
+        public Student (string name, string sNumber)
+        {
+            this.name = name;
+            this.sNumber = sNumber;
+        }
+    }
+}
